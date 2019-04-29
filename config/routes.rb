9d138fda970cb1980eb27f56c043f22674a9a8cb
Yousri <laifayousri@gmail.com>
2019-04-29 17:23:27 +0200

    Modification du fichier routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'gossip/index'
+
+  get 'gossip/show'
+
   get 'user/index'
 
-  get 'user/show'
+  get 'gossip/:id', to: 'user#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
