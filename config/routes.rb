1bde836ca45a9924d77d82a375b2bf5c4bc36cc8
Yousri <laifayousri@gmail.com>
2019-04-29 15:17:49 +0200

    Modification de la route /team

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'team/index'
+  
+  get '/team', to: 'team#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
