609c2cfe231e4bf477b8adcdea28d429ee74fb4f
Yousri <laifayousri@gmail.com>
2019-04-29 19:15:29 +0200

    Modification des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  get '/user/:id', to: 'user#show', as: 'user'
   get '/gossip/:id', to: 'gossip#show', as: 'gossip'
-  get '/:id', to: 'home#index'
   get 'welcome/:id', to: 'welcome#index'
   get '/team', to: 'team#index'
   get '/contact', to: 'contact#index'
