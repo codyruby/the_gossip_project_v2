893275604afb47596dd7773b1b0de11da81e8191
Yousri <laifayousri@gmail.com>
2019-04-29 17:16:41 +0200

    Ajout d'une route /:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/:id', to: 'home#index'
   get 'welcome/:id', to: 'welcome#index'
   get '/team', to: 'team#index'
   get '/contact', to: 'contact#index'
