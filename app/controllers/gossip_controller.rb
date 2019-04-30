cbbdd0245b56adab1e54670f4a91bef233bf51a0
Yousri <laifayousri@gmail.com>
2019-04-30 17:14:10 +0200

    Création des méthode create et new dans le gossip_controller

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -6,4 +6,20 @@ class GossipController < ApplicationController
   def show
     @gossip = Gossip.find(params[:id])
   end
+
+  # Méthode qui permet de créer un nouveau gossip
+  def new
+    @gossip_new = Gossip.new
+  end
+
+  # Méthode qui permet de créer un nouveau gossip avec les params récupéré dans le formulaire de la vue new de gossip
+  def create
+    @gossip_new = Gossip.new(:user_id => params[:user_id], :title => params[:title], :content => params[:content])
+    if @gossip_new.save
+    redirect_to root_path
+    else
+    render 'new'
+    end
+  end
+  
 end
